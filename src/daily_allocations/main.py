1fc08a7d35dbe3669612c2fed5f6ca90ac4c68d3
Alex Turner <none>
2023-02-04 23:49:57 +0000

    Lots of changes!
    Signed-off-by: Alex Turner <none>

--- src/daily_allocations/main.py
+++ src/daily_allocations/main.py
@@ -3,58 +3,51 @@ from datetime import timedelta
 
 import pandas as pd
 from daily_allocations import shift_hours
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, Boolean
 from sqlalchemy import ForeignKey
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, relationship
+
+# Create an engine to carry on with the table. This is the SQLite engine. If echo is True, the log is visible:
+engine = create_engine('sqlite:///allocations_db/test1.db', echo=False)
+
+# Construct a sessionmaker object and bind it to the engine
+Session = sessionmaker(bind=engine)
+session = Session()
 
 # declarative_base() is a factory function that constructs a base class for declarative class definitions.
 Base = declarative_base()
 
 
-# Defining declarative classes. They are assigned to the Base variable. Inside the declarative classes we define the
-# name of the table and the columns with their parameters. The sqlalchemy table and class are associated in the
-# declarative class.
+# Defining declarative classes. They are assigned to the Base variable.
 class StaffTable(Base):
     __tablename__ = 'staff_table'
     id = Column(Integer, primary_key=True)
     name = Column(String)
     role = Column(String)
+    assigned = Column(Boolean, default=False)
 
 
 class PatientTable(Base):
     __tablename__ = 'patient_table'
-
     id = Column(Integer, primary_key=True)
     name = Column(String)
     observation_level = Column(String)
-    room_number = Column(Integer)
+    room_number = Column(String)
 
 
-class StaffObservationsTable(Base):
-    __tablename__ = 'staff_observations_table'
+class StaffOnObsTable(Base):
+    __tablename__ = 'staff_on_obs_table'
     id = Column(Integer, primary_key=True)
-    staff_name = Column, ForeignKey('StaffTable.staff_name')
+    name = Column(String)
+    role = Column(String)
     start_time = Column(String)
     end_time = Column(String)
 
 
-class AllocationsTable(Base):
-    __tablename__ = 'allocations_table'
-    id = Column(Integer, primary_key=True)
-    hours = Column(String)
-
-
-# Create an engine to carry on with the table. This is the SQLite engine. If echo is True, the log is visible:
-engine = create_engine('sqlite:///allocations_db/test.db', echo=False)
-
 # Create all the tables in the database which are defined by Base's subclasses
 Base.metadata.create_all(engine)
-
-# Construct a sessionmaker object and bind it to the engine
-Session = sessionmaker(bind=engine)
-session = Session()
 
 OBS_LEVEL_DICT = {
     1: '60m',
@@ -65,11 +58,11 @@ OBS_LEVEL_DICT = {
     6: 'Seclusion',
     7: 'LTS'
 }
-TIMEBLOCK = timedelta(minutes=30)
-FULL_DURATION = TIMEBLOCK * 25  # 12.5 hours
-MID_DURATION = TIMEBLOCK * 15  # 7.5 hours
-FULL_BREAK_DURATION = TIMEBLOCK * 3  # 1.5 hours
-MID_BREAK_DURATION = TIMEBLOCK * 1  # 30m
+TIME_STAMP = timedelta(minutes=30)
+FULL_DURATION = TIME_STAMP * 25
+MID_DURATION = TIME_STAMP * 15
+FULL_BREAK_DURATION = TIME_STAMP * 3
+MID_BREAK_DURATION = TIME_STAMP * 1
 
 
 def main_menu():
@@ -78,7 +71,8 @@ def main_menu():
     2. Patient Group
     3. Allocate
     4. Staffing Matrix
-    5. Exit
+    5. Working Data
+    6. Exit
     """)
 
     if choice == '1':
@@ -88,9 +82,11 @@ def main_menu():
     elif choice == '3':
         allocations_menu()
     elif choice == '4':
-        print(staffing_matrix.matrix())
+        staffing_matrix.matrix()
         main_menu()
     elif choice == '5':
+        decision_variables_dashboard()
+    elif choice == '6':
         quit()
 
 
@@ -109,7 +105,7 @@ def patient_menu():
 """)
     choice = int(input('Make selection: '))
     if choice == 1:
-        print(patient_table())
+        print(patients_df())
         patient_menu()
     elif choice == 2:
         create_patient()
@@ -138,7 +134,7 @@ def staff_menu():
 """)
     choice = int(input('Make selection: '))
     if choice == 1:
-        print(staff_table())
+        print(staff_team_df())
         staff_menu()
     elif choice == 2:
         create_staff()
@@ -150,17 +146,17 @@ def staff_menu():
 
 def allocations_menu():
     print("""
-    1. Show allocations table
-    2. Add staff
-    3. Modify and/or delete staff
+    1. Observation levels
+    2. Assign staff
+    3. Under development
     4. Return to main menu
 """)
     choice = int(input('Make selection: '))
     if choice == 1:
-        print(create_allocations())
+        print(observations_df())
         allocations_menu()
     elif choice == 2:
-        staff_on_obs_table()
+        assign_staff_to_obs()
     elif choice == 3:
         print('under development')
         allocations_menu()
@@ -196,12 +192,10 @@ def create_patient():
 
 
 def update_patient():
-    print(patient_table())
-
+    print(patients_df())
     patient_id_to_modify = int(input('enter ID of patient to modify: '))
-    patient_to_modify = session.query(PatientTable).get(patient_id_to_modify)
-    print(f"Name: {patient_to_modify.room_number} {patient_to_modify.name} "
-          f"{patient_to_modify.observation_level} will be modified")
+    patient_to_modify = session.query(PatientTable).filter_by(id=patient_id_to_modify).first()
+    print(f"{patient_to_modify.name} will be modified")
     print("""
     1. Room number
     2. Name
@@ -209,12 +203,12 @@ def update_patient():
     4. Delete Patient
     5. Return to main patient menu
     """)
-    item_to_modify = int(input())
 
+    item_to_modify = int(input())
     if item_to_modify == 1:
         patient_to_modify.room_number = int(input('Room Number: '))
     elif item_to_modify == 2:
-        patient_to_modify.patient_name = (input('Patient Name: '))
+        patient_to_modify.name = input('Patient Name: ')
     elif item_to_modify == 3:
         print(display_obs_levels_key())
         patient_to_modify.observation_level = OBS_LEVEL_DICT[int(input('Observation Level: '))]
@@ -249,7 +243,7 @@ input, the function moves on to the next step.
 Finally, the function uses the session.commit() method to save the changes to the database and commit the transaction.
 It then calls the patient_menu() function.
     """
-    print(patient_table())
+    print(patients_df())
 
     patient_id_to_delete = int(input('enter ID of patient to delete: '))
     patient_to_delete = session.query(StaffTable).get(patient_id_to_delete)
@@ -280,10 +274,10 @@ def create_staff():
 
 
 def update_or_delete_staff():
-    print(staff_table())
+    print(staff_team_df())
     staff_id_to_update = int(input('enter ID of staff to update: '))
-    staff_to_update = session.query(StaffTable).get(staff_id_to_update)
-    print(f"Name: {staff_to_update.name} | {staff_to_update.role} will be updated")
+    staff_to_update = session.query(StaffTable).filter_by(id=staff_id_to_update).first()
+    print(f"{staff_to_update.name} will be updated")
     print("""
     1. Update Name
     2. Update Role 
@@ -301,10 +295,9 @@ def update_or_delete_staff():
         staff_to_update.role = input('Role: ')
     # delete staff
     elif update_menu_selection == 3:
-        staff_to_delete = session.query(StaffTable).get(staff_to_update)
-        check = input(f"Delete: {staff_to_delete.name} y/N: ")
+        check = input(f"Delete: {staff_to_update.name} y/N: ")
         if check == 'y':
-            session.delete(staff_to_delete)
+            session.delete(staff_to_update)
         else:
             update_or_delete_staff()
     # menus
@@ -317,7 +310,31 @@ def update_or_delete_staff():
     update_or_delete_staff()
 
 
-def staff_table():
+def assign_staff_to_obs():
+    query = session.query(StaffTable)
+    all_rows = query.all()
+    print('\n', staff_team_df())
+
+    select_staff = [staff_id for staff_id in input('\nID of staff to assign on obs: ').split()]
+    selected_staff = [session.query(StaffTable).get(staff_id) for staff_id in select_staff]
+
+    for staff in all_rows:
+        if staff in selected_staff:
+            staff.assigned = True
+        else:
+            staff.assigned = False
+    session.commit()
+
+    print('\nAssigned to undertake observations:')
+    assigned_query = session.query(StaffTable).filter_by(assigned=True)
+    for n, staff in enumerate(assigned_query):
+        print(f'{n + 1}. {staff.name} ({staff.role})')
+    print('\n', staff_team_df())
+    main_menu()
+    return assigned_query
+
+
+def staff_team_df():
     """
 This function creates a pandas DataFrame from the StaffTable object of an SQLAlchemy session.
 The DataFrame contains the staff's name, role, hours, and ID, and is sorted by role.
@@ -325,13 +342,13 @@ The function returns the DataFrame.
 """
     query = session.query(StaffTable)
     all_rows = query.all()
-    data = [[row.name, row.role, row.id] for row in all_rows]
-    df = pd.DataFrame(data, columns=['Name', 'Role', 'ID'])
-    staff_group = df.set_index('ID').sort_values(by='Role')
-    return staff_group
+    data = [[row.id, row.name, row.role, row.assigned] for row in all_rows]
+    df = pd.DataFrame(data, columns=['ID', 'Name', 'Role', 'Assigned on obs'])
+    all_staff = df.set_index('ID').sort_values(by='Role')
+    return all_staff
 
 
-def patient_table():
+def patients_df():
     """
     Here's what the function is doing
 1.    The function starts by creating a query from the PatientTable class in the session.
@@ -342,13 +359,13 @@ def patient_table():
     """
     query = session.query(PatientTable)
     all_rows = query.all()
-    data = [[row.room_number, row.name, row.observation_level] for row in all_rows]
-    df = pd.DataFrame(data, columns=['Room Num', 'Patient Name', 'Observation Level'])
-    patients = df.set_index('Room Num').sort_values(by='Room Num')
+    data = [[row.id, row.name, row.observation_level, row.room_number] for row in all_rows]
+    df = pd.DataFrame(data, columns=['id', 'Patient Name', 'Observation Level', 'Room Num'])
+    patients = df.set_index('id').sort_values(by='Room Num')
     return patients
 
 
-def create_allocations():
+def observations_df():
     # Select patients according to observation level
     obs_query_60m = session.query(PatientTable).filter(PatientTable.observation_level == '60m')
     obs_query_30m = session.query(PatientTable).filter(PatientTable.observation_level == '30m')
@@ -359,73 +376,24 @@ def create_allocations():
     lts_obs_query = session.query(PatientTable).filter(PatientTable.observation_level == 'LTS')
 
     # Pull out patient details from each respective row.
-    obs_60m = [[patient.name, patient.observation_level] for patient in obs_query_60m]
-    obs_30m = [[patient.name, patient.observation_level] for patient in obs_query_30m]
-    obs_15m = [[patient.name, patient.observation_level] for patient in obs_query_15m]
-    obs_one_2_one = [[patient.name, patient.observation_level] for patient in one_2_one_obs_query]
-    obs_two_2_one = [[patient.name, patient.observation_level] for patient in two_2_one_obs_query]
-    obs_seclusion = [[patient.name, patient.observation_level] for patient in seclusion_obs_query]
-    obs_lts = [[patient.name, patient.observation_level] for patient in lts_obs_query]
+    obs_60m = [[patient.name, patient.observation_level, patient.room_number] for patient in obs_query_60m]
+    obs_30m = [[patient.name, patient.observation_level, patient.room_number] for patient in obs_query_30m]
+    obs_15m = [[patient.name, patient.observation_level, patient.room_number] for patient in obs_query_15m]
+    obs_one_2_one = [[patient.name, patient.observation_level, patient.room_number] for patient in one_2_one_obs_query]
+    obs_two_2_one = [[patient.name, patient.observation_level, patient.room_number] for patient in two_2_one_obs_query]
+    obs_seclusion = [[patient.name, patient.observation_level, patient.room_number] for patient in seclusion_obs_query]
+    obs_lts = [[patient.name, patient.observation_level, patient.room_number] for patient in lts_obs_query]
+    data = obs_60m + obs_30m + obs_15m + obs_one_2_one + obs_two_2_one + obs_seclusion + obs_lts
+    df = pd.DataFrame(data, columns=['Name', 'Observation Level', 'Room No.'])
+    # df.set_index('Room No.', inplace=True)
+    df.sort_values(by='Room No.', ascending=True, inplace=True)
 
-    # Calculate number of TIMEBLOCKS required to cover all level of obs
-    total_timeblocks = FULL_DURATION  # 25 (12.5h) to cover 60m, 30m, 15m obs
-    obs_list = [obs_60m, obs_30m, obs_15m, obs_one_2_one, obs_two_2_one, obs_seclusion, obs_lts]
-    for obs in obs_list[3:]:
-        total_timeblocks += (TIMEBLOCK * 25) * len(obs)
-    print(total_timeblocks / TIMEBLOCK)
+    print(df)
 
-    # Creates a list of dictionaries. One dictionary for each patient on obs. Dictionary keys are each patient's name
-    # and obs level. The dictionary value for all dictionaries is a list of 24 None items. The key
-    # provides the column label. The values will be updated with staff allocated for each half hour during
-    # the shift for each patient on obs.
-
-    import itertools
-    # This code flattens a list of lists (obs_list) into a single list (flattened_list) using the itertools.chain()
-    # function and the list() function. The * operator is used to unpack the elements of obs_list[1:] which are passed
-    # as separate arguments to the itertools.chain() function. This creates an iterator that concatenates the elements
-    # of all the sublists, which is then converted to a list and stored in flattened_list.
-    flattened_list = list(itertools.chain(*obs_list[1:]))
-    print(flattened_list)
-
-    patient_dict_holder = [{patient[0] + ' | ' + patient[1]: [None for n in range(24)]} for patient in flattened_list]
-
-    # check whether allocating for day or night shift. Check required to select the correct sequence of hours
-    shift = input('Generate allocations for days or nights d/N: ')
-    if shift == 'd':
-        data = {'Time': shift_hours.ld_hours()}
-    else:
-        data = {'Time': shift_hours.n_hours()}
-
-    # Generate allocations table.
-    for patient_dict in patient_dict_holder:
-        data.update(patient_dict)
-        allocation_table = pd.DataFrame(data)
-    print(allocation_table.to_string(index=False))
     main_menu()
-    return create_allocations
 
 
-def staff_on_obs_table():
-    staff_hours_dict = {}
-    print(staff_table())
-    select_staff = input('\nID of staff to add to allocations: ').split()
-    select_staff = [int(staff_id) for staff_id in select_staff]
-
-    selected_staff = [session.query(StaffTable).get(staff_id) for staff_id in select_staff]
-    define_shift = input('Day or Night shift d/n: ')
-    if define_shift == 'd':
-        hours = shift_hours.ld_hours()
-    elif define_shift == 'n':
-        hours = shift_hours.n_hours()
-
-    print('Staff selected to complete observations')
-    for staff in selected_staff:
-        staff_hours_dict.update({staff.name: hours})
-
-    print(staff_hours_dict)
-
-
-def working_data():
+def decision_variables_dashboard():
     totals = {
         'patients_on_ward': session.query(PatientTable).count(),
         'number_on_60s':    session.query(PatientTable).filter(PatientTable.observation_level == '60m').count(),
@@ -435,13 +403,14 @@ def working_data():
         'number_on_2:1':    session.query(PatientTable).filter(PatientTable.observation_level == '2:1').count(),
         'number_in_SEC':    session.query(PatientTable).filter(PatientTable.observation_level == 'Seclusion').count(),
         'number_in_LTS':    session.query(PatientTable).filter(PatientTable.observation_level == 'LTS').count(),
+        'assigned_staff':   session.query(StaffTable).filter_by(assigned=True)
     }
 
     generals = 12
     total_obs_hours = totals['number_on_1:1'] * 12 + totals['number_on_2:1'] * 24 + totals['number_in_SEC'] * 12 + \
                       totals['number_in_LTS'] * 12 + generals
 
-    print(f"Number of Patients on Ward: {totals['patients_on_ward']}\n")
+    print(f"\nNumber of Patients on Ward: {totals['patients_on_ward']}")
     print(f"Number on 60m observations: {totals['number_on_60s']}")
     print(f"Number on 30m observations: {totals['number_on_30s']}")
     print(f"Number on 15m observations: {totals['number_on_15s']}")
@@ -450,10 +419,15 @@ def working_data():
     print(f"Number in seclusion: {totals['number_in_SEC']}")
     print(f"Number in LTS: {totals['number_in_LTS']}\n")
     print(f'Total enhanced observation hours: {total_obs_hours - generals}')
-    staffing_matrix.matrix(total_obs_hours, totals['patients_on_ward'])
+    staffing_matrix.calculate_staff_numbers(total_obs_hours, totals['patients_on_ward'])
+    print('\nStaff selected to complete observations')
+    for n, staff in enumerate(totals['assigned_staff']):
+        print(f'{n + 1}. {staff.name} ({staff.role})')
+
     main_menu()
 
-    return totals
+    return tc
+
 
 if __name__ == '__main__':
-    working_data()
+    main_menu()

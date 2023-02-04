137f9347224b1d24ce9bc00591468e124d516a0f
Alex Turner <none>
2023-02-04 23:51:18 +0000

    Rewrote ui prompts, functions and logic to calculate duration between start and end times.
    Signed-off-by: Alex Turner <none>

--- src/daily_allocations/shift_hours.py
+++ src/daily_allocations/shift_hours.py
@@ -1,99 +1,80 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 
-NOW = datetime.now()
+
+def create_shift_list(start_time, end_time):
+    """
+    Given start_time and end_time in string format of 'HH:MM', returns a list of
+    half-hourly time stamps between the two times.
+
+    Example:
+    >> create_shift_list('08:00', '20:00')
+    ['08:00', '08:30', '09:00', ..., '19:30', '20:00']
+
+    Args:
+    - start_time (str): start time in string format of 'HH:MM'
+    - end_time (str): end time in string format of 'HH:MM'
+
+    Returns:
+    - list of half-hourly time stamps between start_time and end_time
+    """
+    start_time = datetime.strptime(start_time, "%H:%M")
+    end_time = datetime.strptime(end_time, "%H:%M")
+
+    # Add 1 day to end_time if start_time is after end_time
+    if start_time > end_time:
+        end_time = end_time + timedelta(days=1)
+
+    duration = end_time - start_time
+    total_minutes = int(duration.total_seconds() / 60)
+    half_hours = total_minutes // 30
+    shift_hours = []
+    current_time = start_time
+
+    for i in range(half_hours + 1):
+        shift_hours.append(current_time.strftime('%H:%M'))
+        current_time += timedelta(minutes=30)
+        if current_time > end_time:
+            break
+    return shift_hours
+
+
+def get_and_show_shift_hours(start_time, end_time):
+    shift_hours = create_shift_list(start_time, end_time)
+    # print("Start time:", start_time)
+    # print("End time:", end_time)
+
+    start_time = datetime.strptime(start_time, "%H:%M")
+    end_time = datetime.strptime(end_time, "%H:%M")
+
+    # Add 1 day to end_time if start_time is after end_time
+    if start_time > end_time:
+        end_time = end_time + timedelta(days=1)
+
+    duration = end_time - start_time
+    total_seconds = duration.total_seconds()
+    hours, remainder = divmod(int(total_seconds), 3600)
+    minutes, _ = divmod(remainder, 60)
+    # print("Duration: {} hours {} minutes".format(hours, minutes))
+    # print("Shift hours:", shift_hours)
+    return shift_hours
 
 
 def ld_hours():
-    long_day_times = []
-    hour = 8
-    minute = [0, 30]
-    even_odd = 1
-
-    for i in range(24):
-        if even_odd % 2 != 0:
-            choice = 0
-        else:
-            choice = 1
-        t = datetime(year=NOW.year, month=NOW.month, day=NOW.day, hour=hour, minute=minute[choice],
-                     microsecond=NOW.microsecond)
-        long_day_times.append(t.strftime('%H:%M'))
-        even_odd += 1
-
-        if even_odd % 2 != 0:
-            hour += 1
-
-    return long_day_times
+    st = '08:00'
+    et = '20:00'
+    return create_shift_list(st, et)
 
 
 def n_hours():
-    night_times = []
-    hour = 20
-    minute = [0, 30]
-    even_odd = 1
-
-    for i in range(24):
-        if even_odd % 2 != 0:
-            choice = 0
-        else:
-            choice = 1
-        t = datetime(year=NOW.year, month=NOW.month, day=NOW.day, hour=hour, minute=minute[choice],
-                     microsecond=NOW.microsecond)
-        night_times.append(t.strftime('%H:%M'))
-
-        even_odd += 1
-        if even_odd % 2 != 0:
-            hour += 1
-        if hour > 23:
-            hour -= 24
-
-    return night_times
+    st = '20:00'
+    et = '08:00'
+    get_and_show_shift_hours(st, et)
 
 
 def custom_hours():
-    from datetime import datetime
-
     st = input("Enter start time in 24h format (HH:MM): ")
     et = input("Enter end time in 24h format (HH:MM): ")
-
-    # convert input to datetime objects
-    st = datetime.strptime(st, "%H:%M")
-    et = datetime.strptime(et, "%H:%M")
-
-    # calculate and round the duration to hours and minutes
-    d = et - st
-    total_seconds = d.total_seconds()
-    hours, remainder = divmod(int(total_seconds), 3600)
-    minutes, seconds = divmod(remainder, 60)
-
-    # format datetime objects to only include hours and minutes
-    st_formatted = st.strftime("%H:%M")
-    et_formatted = et.strftime("%H:%M")
-
-    # print values
-    print("Start time:", st_formatted)
-    print("End time:", et_formatted)
-    print("Duration: {} hours {} minutes".format(hours, minutes))
-
-    # mid_times = []
-    # hour = int(input('Start time: '))
-    # minute = [0, 30]
-    # even_odd = 1
-    #
-    # for i in range(16):
-    #     if even_odd % 2 != 0:
-    #         choice = 0
-    #     else:
-    #         choice = 1
-    #
-    #     t = datetime(year=NOW.year, month=NOW.month, day=NOW.day, hour=hour, minute=minute[choice],
-    #                  microsecond=NOW.microsecond)
-    #     mid_times.append(t.strftime('%H:%M'))
-    #     even_odd += 1
-    #
-    #     if even_odd % 2 != 0:
-    #         hour += 1
-    #     if hour > 23:
-    #         hour -= 24
+    get_and_show_shift_hours(st, et)
 
 
-custom_hours()
+

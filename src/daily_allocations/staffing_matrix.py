48a430146554ec04105b79157d5d0ceb1df7615b
Alex Turner <none>
2023-02-10 17:46:22 +0000

    split functions/return values.
    Signed-off-by: Alex Turner <none>

--- src/daily_allocations/staffing_matrix.py
+++ src/daily_allocations/staffing_matrix.py
@@ -18,14 +18,13 @@ def matrix():
                        [14, 2.5, 3, 2, 2],
                        [15, 2.5, 4, 2, 2],
                        [16, 2.5, 4, 2, 2]]
-    print('\nSaltwood Staffing Matrix\n')
-    print(pd.DataFrame(staffing_matrix, columns=['Number of Patients', 'Day RMN', 'Day SW', 'Night RMN', 'Night SW']))
-    return staffing_matrix
+
+    sm_df = pd.DataFrame(staffing_matrix, columns=['Number of Patients', 'Day RMN', 'Day SW', 'Night RMN', 'Night SW'])
+    return staffing_matrix, sm_df
 
 
 def calculate_staff_numbers(obs_hours=0, patients_on_ward=0):
-    sm = matrix()
-
+    sm, sm_df = matrix()
     extra_staff_needed = 0
     if obs_hours > 24 and patients_on_ward >= 11:
         difference = obs_hours - 24
@@ -47,5 +46,5 @@ def calculate_staff_numbers(obs_hours=0, patients_on_ward=0):
 
     number_of_patients, day_rmn, day_sw, night_rmn, night_sw = sm[patients_on_ward - 1]
     print(f'\nDay RMN: {day_rmn}\nDay HCA: {day_sw}\nNight RMN: {night_rmn}\nNight HCA: {night_sw}')
-    print(matrix_df.set_index('Number of Patients'))
+
     return matrix_df.set_index('Number of Patients')
